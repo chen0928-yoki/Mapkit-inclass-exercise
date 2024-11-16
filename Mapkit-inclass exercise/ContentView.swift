91ae3578f21f777ebcc9ddb3ec565761dec2850b
Qianyu Chen <chen0928@algonquinlive.com>
2024-11-16 10:31:07 -0500

    Initial Commit

---
+++ Mapkit-inclass exercise/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Mapkit-inclass exercise
+//
+//  Created by 入江川玖 on 2024-11-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Mapkit-inclass exercise/Mapkit_inclass_exerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mapkit_inclass_exerciseApp.swift
+//  Mapkit-inclass exercise
+//
+//  Created by 入江川玖 on 2024-11-16.
+//
+
+import SwiftUI
+
+@main
+struct Mapkit_inclass_exerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
